71651196482a7f7789571e2c41fae6e900f2ffcc
Alotritne <alotritne@gmail.com>
2021-09-30 13:43:15 +0000

    first commit

---
+++ modules/commands/ad.js
@@ -0,0 +1,11 @@
+﻿module.exports.config = {
+	name: "ad",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Hà Mạc Trường Giang",
+	description: "Thông tin về admin",
+	commandCategory: "Thông tin về admin",
+	cooldowns: 0
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`\n★Thông Tin Admin Bot Này★\nADMIN NAME : Nguyễn Minh Triết\nBiệt Danh: Tritt ~~\nGiới Thiệu: Nhà anh không có gì ngoài nợ ngân hàng 10 tỉ\nI Have Something To You\nI Love You\nTính Cách : Cái gì cái chứ trêu gái là nhanh nhất nhé OwO\nChiều cao : 1m5\nSinh ngày : 06/06/2006\nLiên hệ:+84396019687 \nInstagram: iammtrit\nTikTok: iammtrit\nSở Thích: Thích chơi game, trêu gái\nUID FACEBOOK : 100045403026854\nName ID : Nguyễn Minh Triết\nLink Facebook : https://www.facebook.com/alotritne\nVài lời tới người dùng BOT: Vui lòng không spam khi sử dụng để tránh die bot. Cảm ơn mọi người đã sử dụng đên con bot của mình.\nLưu ý : Đừng có dại dột mà add 2 bot kẻo bị phát hiện là bạn toang đó <3\nCảnh báo : Vui lòng không dùng bot với mục đích xấu hay cố ý report acc facebook\nChúc bạn sử dụng vui vẻ <3\n=== Nguyễn Minh Triết ===`, event.threadID, event.messageID);
